f2ec5638c4321cfeaab9c46c3c4dbcb697583152
Ran Kim <rannio@gmail.com>
2022-05-13 10:40:32 -0400

    created get all music services

--- src/services/UserServices.js
+++ src/services/UserServices.js
@@ -8,3 +8,22 @@ export const GetAllUsers = async () => {
         throw error
     }
 }
+
+export const GetIndUser = async (data) => {
+    try {
+        const res = await Client.get(`/users/${data}`)
+        return res
+    } catch (error) {
+        throw error
+    }
+}
+
+export const CreateMusic = async (data) => {
+    try {
+        const res = await Client.post(`/users/newmusic/${data}`)
+        return res
+    } catch (error) {
+        throw error
+    }
+}
+
